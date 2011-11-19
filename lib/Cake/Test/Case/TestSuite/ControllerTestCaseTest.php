d6e66239f8981a747566b658b25d08225e78863e
laander <la@laander.com>
2011-11-19 18:11:53 +0100

    made README.md setup guide, addemissing app/tmp/ folders, schema script now also creates default users

--- lib/Cake/Test/Case/TestSuite/ControllerTestCaseTest.php
+++ lib/Cake/Test/Case/TestSuite/ControllerTestCaseTest.php
@@ -426,6 +426,21 @@ class ControllerTestCaseTest extends CakeTestCase {
 	}
 
 /**
+ * Test that REST actions with XML/JSON input work.
+ *
+ * @return void
+ */
+	public function testTestActionJsonData() {
+		$result = $this->Case->testAction('/tests_apps_posts/input_data', array(
+			'return' => 'vars',
+			'method' => 'post',
+			'data' => '{"key":"value","json":true}'
+		));
+		$this->assertEquals('value', $result['data']['key']);
+		$this->assertTrue($result['data']['json']);
+	}
+
+/**
  * Tests autoMock ability
  */
 	public function testAutoMock() {
@@ -471,6 +486,7 @@ class ControllerTestCaseTest extends CakeTestCase {
  * @return void
  */
 	public function testNoControllerReuse() {
+		$this->Case->autoMock = true;
 		$result = $this->Case->testAction('/tests_apps/index', array(
 			'data' => array('var' => 'first call'),
 			'method' => 'get',
@@ -494,6 +510,19 @@ class ControllerTestCaseTest extends CakeTestCase {
 			'return' => 'contents'
 		));
 		$this->assertContains('third call', $result);
+	}
+
+/**
+ * Test that multiple calls to redirect in the same test method don't cause issues.
+ *
+ * @return void
+ */
+	public function testTestActionWithMultipleRedirect() {
+		$Controller = $this->Case->generate('TestsApps');
+
+		$options = array('method' => 'get');
+		$this->Case->testAction('/tests_apps/redirect_to', $options);
+		$this->Case->testAction('/tests_apps/redirect_to', $options);
 	}
 
 }
